496176a4371a4d674f8f91a4c6d7f7fac1915800
Motti789 <mchillywilly@gmail.com>
2022-11-09 10:30:45 -0500

    added some dat to the seed file for testing the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Article.destroy_all
+# Comment.destroy_all
+
+article1 = Article.create(title: "My first article", body: "Here is where I can write all of my blog posts")
+article2 = Article.create(title: "And here is my second", body: "This is really do much fun!")
+
+comment1 = Comment.create(commenter: "Motti Wilschanski", body: "That's a great article!", article_id: article1.id)
+comment2 = Comment.create(commenter: "Shmuli Wilschanski", body: "And your on the second already!", article_id: article2.id)
